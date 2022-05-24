eed8c26892a61ebd75439bf146ee9f9e29cba323
Kritbovorn <ktaweeyossak@yahoo.com>
2022-05-24 23:41:11 +0700

    1:12:00: about express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!!!');
+
+const express = require('express');
+
+// 127.0.0.1
+const PORT = 3000;
+
+const app = express();
+
+
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log('### Connect at port ' + PORT);
+    console.log(`### Connect at port ${PORT}`);
+
+});
+
